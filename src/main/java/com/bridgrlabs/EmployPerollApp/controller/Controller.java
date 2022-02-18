1764df7dd171cab2210d488fec62af1186e0cb83
mukulBillore <mukulbillore88@gmail.com>
2022-02-19 00:28:47 +0530

    Modified : modified and beautify the code

--- src/main/java/com/bridgrlabs/EmployPerollApp/controller/Controller.java
+++ src/main/java/com/bridgrlabs/EmployPerollApp/controller/Controller.java
@@ -18,30 +18,34 @@ import com.bridgrlabs.EmployPerollApp.service.EmployService;
 public class Controller {
 	@Autowired
 	EmployService service;
-
+ 
+	//showing hello msg
 	@GetMapping("/")
 	public String test() {
 		return "hello dear ! welcome to employ paroll app ";
 	}
-
+	//showing hello msg by name
 	@PutMapping("/hello")
 	public ResponseEntity<String> sayHelloToUserOFWithName(@RequestParam int id) {
 		String msg = "Hello " + service.getUserName() + "  to EmployParollApp";
 		return new ResponseEntity<String>(msg, HttpStatus.OK);
 	}
 	
+	//saving the employ data 
 	@PostMapping("/setuser")
 	public ResponseEntity<String> saveUserdata(@RequestBody EmployModel employ ) {
 		service.saveEmploy(employ);
 		return new ResponseEntity<String>("sucussfully saved user", HttpStatus.OK);
 	}
 	
+	// getting the employ data
 	@GetMapping("/getuser/{id}")
 	public ResponseEntity<EmployModel> retriveUserdata(@PathVariable Integer id ) {
 		EmployModel emp = service.getEmploy(id);
 		return new ResponseEntity<EmployModel>(emp, HttpStatus.OK);
 	}
 	
-//update by id 
-	//delete by id
+//update the employ by id
+	
+//delete the employ by id
 }
