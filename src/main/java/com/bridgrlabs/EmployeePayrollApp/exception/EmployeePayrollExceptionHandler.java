2d4df70cf95ac16984fe6e8597da23d59cf1b595
mukulBillore <mukulbillore88@gmail.com>
2022-02-22 22:12:21 +0530

    updated : provided user friendly case response if the validation fails

---
+++ src/main/java/com/bridgrlabs/EmployeePayrollApp/exception/EmployeePayrollExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.bridgrlabs.EmployeePayrollApp.exception;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.bridgrlabs.EmployeePayrollApp.dto.ResponceDTO;
+
+@ControllerAdvice
+public class EmployeePayrollExceptionHandler {
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ResponceDTO> handlerMethodArgumentNotValidException(
+			MethodArgumentNotValidException exception) {
+
+		List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
+		List<String> errMesg = errorList.stream().map(objErr -> objErr.getDefaultMessage())
+				.collect(Collectors.toList());
+
+		ResponceDTO responseDTO = new ResponceDTO("Exception while processing REST requests", errMesg);
+		ResponseEntity<ResponceDTO> responce= new ResponseEntity<ResponceDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+		return responce;
+	}
+}
